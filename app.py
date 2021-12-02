f6e4dc4702b1af8ed253f099cd3a4d4ad02215df
ahmed <ahmedtilal1@gmail.com>
2021-12-02 18:37:35 +0000

    Fix dummy api bug

--- app.py
+++ app.py
@@ -10,7 +10,9 @@ schema = {'properties': {
 'profile_image': {'type': 'string'}}}
 
 #Here we make the HTTP request and parse the response
-with urllib.request.urlopen('http://dummy.restapiexample.com/api/v1/employees') as response:
+headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3"}
+req = urllib.request.Request(url= 'http://dummy.restapiexample.com/api/v1/employees', headers = headers)
+with urllib.request.urlopen(req) as response:
     emp_data = json.loads(response.read().decode('utf-8'))
 #next we call singer.write_schema which writes the schema of the employees stream
 singer.write_schema('employees', schema, 'id')
